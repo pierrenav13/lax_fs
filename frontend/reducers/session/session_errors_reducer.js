1b2d331ab86cd10edb5a6175d777f07bae59f82c
pierre <pnavarin@gmail.com>
2022-07-19 14:06:11 -0400

    Login

--- frontend/reducers/session/session_errors_reducer.js
+++ frontend/reducers/session/session_errors_reducer.js
@@ -4,7 +4,7 @@ const sessionErrorsReducer = (state = [], action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_SESSION_ERRORS:
-            return Object.assign({}, state, action.errors);
+            return action.errors;
         case RECEIVE_CURRENT_USER:
             return [];
         case PURGE_ERRORS:
