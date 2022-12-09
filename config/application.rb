4d16610e0ede4370539d79110e01753dbb8d6983
pierre <pnavarin@gmail.com>
2022-12-09 15:29:01 -0500

    Heroku

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Lax
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.railties_order = [:all, :main_app]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
