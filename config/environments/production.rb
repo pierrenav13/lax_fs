5be6389d6d007d7b00257b32b0e063811fa7747d
pierre <pnavarin@gmail.com>
2022-12-21 15:08:26 -0500

    compressor fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,10 +26,10 @@ Rails.application.configure do
   # config.assets.js_compressor = :terser
   # config.assets.css_compressor = :sass
 
-  config.assets.configure do |env|
-    env.js_compressor  = :terser
-    # env.css_compressor = :sass   # or :yui
-  end
+  # config.assets.configure do |env|
+  #   env.js_compressor  = :terser
+  #   # env.css_compressor = :sass   # or :yui
+  # end
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
